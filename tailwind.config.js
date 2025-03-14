ac8fb917f874ceb82cce1c0a97ec6a48dac127c7
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-03-14 12:31:01 +0300

    Revised dependenciew

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // Include all files
 
   theme: {
-    extend: {},
+    extend: { 
+      keyframes: {
+      fadeInUp: {
+        '0%': { opacity: '0', transform: 'translateY(20px)' },
+        '100%': { opacity: '1', transform: 'translateY(0)' },
+      },
+    },
+    animation: {
+      fadeInUp: 'fadeInUp 0.5s ease-out forwards',
+    },},
   },
   plugins: [],
 }
